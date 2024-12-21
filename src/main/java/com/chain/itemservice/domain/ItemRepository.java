175d7e721aae284ed87cb5a683d540c446cb3f8c
이승환 <seaung13@gmail.com>
2024-12-22 02:03:49 +0900

    feat: 상품 도메인 개발

---
+++ src/main/java/com/chain/itemservice/domain/ItemRepository.java
@@ -0,0 +1,40 @@
+package com.chain.itemservice.domain;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private final static Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Item save(Item item) {
+        item.setId(sequence++);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long id, Item updateItem) {
+        Item findItem = findById(id);
+        findItem.setItemName(updateItem.getItemName());
+        findItem.setPrice(updateItem.getPrice());
+        findItem.setQuantity(updateItem.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
---
+++ src/test/java/com/chain/itemservice/domain/ItemRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.chain.itemservice.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void tearDown() {
+        itemRepository.clearStore();
+    }
+
+    @DisplayName("상품을 저장할 수 있다")
+    @Test
+    void save() {
+        // given
+        Item item = new Item("itemA", 10000, 10);
+
+        // when
+        Item savedItem = itemRepository.save(item);
+
+        // then
+        assertThat(itemRepository.findById(savedItem.getId())).isEqualTo(savedItem);
+    }
+
+    @DisplayName("저장된 모든 상품을 조회할 수 있다")
+    @Test
+    void findAll() {
+        // given
+        Item itemA = new Item("itemA", 10000, 10);
+        Item itemB = new Item("itemB", 20000, 20);
+        itemRepository.save(itemA);
+        itemRepository.save(itemB);
+
+        // when
+        List<Item> items = itemRepository.findAll();
+
+        // then
+        assertThat(items).contains(itemA, itemB);
+        assertThat(items.size()).isEqualTo(2);
+    }
+
+    @DisplayName("저장된 상품의 id를 알면 해당 상품을 수정할 수 있다.")
+    @Test
+    void update() {
+        // given
+        Item itemA = new Item("itemA", 10000, 10);
+        Item savedItem = itemRepository.save(itemA);
+
+        // when
+        itemRepository.update(savedItem.getId(),
+                new Item("itemB", 20000, 20));
+
+        // then
+        assertThat(savedItem.getItemName()).isEqualTo("itemB");
+        assertThat(savedItem.getPrice()).isEqualTo(20000);
+        assertThat(savedItem.getQuantity()).isEqualTo(20);
+    }
+}
