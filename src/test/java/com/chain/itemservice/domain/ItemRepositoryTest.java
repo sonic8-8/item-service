fc3ea8ca1dad30b4c84c854e7bb68942a45e02d6
이승환 <seaung13@gmail.com>
2024-12-22 02:22:54 +0900

    feat: 상품 서비스 HTML

--- src/test/java/com/chain/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/com/chain/itemservice/domain/ItemRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.chain.itemservice.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
