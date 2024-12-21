ffb68deacbfe9334f69969f7016238379082775b
이승환 <seaung13@gmail.com>
2024-12-22 02:49:09 +0900

    feat: 상품 목록 - 타임리프

---
+++ src/main/java/com/chain/itemservice/web/item/basic/BasicItemController.java
@@ -0,0 +1,33 @@
+package com.chain.itemservice.web.item.basic;
+
+import com.chain.itemservice.domain.Item;
+import com.chain.itemservice.domain.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
+    }
+}
