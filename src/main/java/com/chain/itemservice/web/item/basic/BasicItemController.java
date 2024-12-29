c281eb76ea178c518fb485a922b2db9191fb2896
이승환 <seaung13@gmail.com>
2024-12-29 19:00:04 +0900

    feat: 입력 폼 처리 - th:object, th:field

--- src/main/java/com/chain/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/com/chain/itemservice/web/item/basic/BasicItemController.java
@@ -33,7 +33,8 @@ public class BasicItemController {
     }
 
     @GetMapping("/add")
-    public String addForm() {
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
         return "basic/addForm";
     }
 
