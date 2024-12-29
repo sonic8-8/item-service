a9e5051bad49949f479b8a3a74f0f7ace029a404
이승환 <seaung13@gmail.com>
2024-12-30 02:14:50 +0900

    feat: 체크 박스 - 멀티

--- src/main/java/com/chain/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/com/chain/itemservice/web/item/basic/BasicItemController.java
@@ -10,7 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,6 +21,15 @@ import java.util.List;
 public class BasicItemController {
 
     private final ItemRepository itemRepository;
+
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -82,6 +93,7 @@ public class BasicItemController {
         redirectAttributes.addAttribute("status", true);
 
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
 
         return "redirect:/basic/items/{itemId}";
     }
